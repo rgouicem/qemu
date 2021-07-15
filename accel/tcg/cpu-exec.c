dde64b9960d52d5614ccd209ced6456ddf99d94e
Redha Gouicem <redha.gouicem@gmail.com>
2021-07-15 11:56:04 +0200

    misc

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -439,10 +439,12 @@ static inline TranslationBlock *tb_find(CPUState *cpu,
     uint32_t flags;
 
     cpu_get_tb_cpu_state(env, &pc, &cs_base, &flags);
-    fprintf(stderr, "%s:%d: pc=" TARGET_FMT_lx " no fence\n",
-            __func__, __LINE__, pc);
 
-    if (is_bb_shared(pc)) {
+    fprintf(stderr, "%s:%d: pc=" TARGET_FMT_lx " cflags[CF_PARALLEL]=%x flags[CF_PARALLEL]=%x\n",
+            __func__, __LINE__, pc, cflags & CF_PARALLEL, flags & CF_PARALLEL);
+
+    /* if (is_bb_shared(pc)) { */
+    if (0) {
         cflags = cflags | CF_PARALLEL;
         /* fprintf(stderr, "%s:%d: pc=" TARGET_FMT_lx " no fence\n", */
         /*         __func__, __LINE__, pc); */
